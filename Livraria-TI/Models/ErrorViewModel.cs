542fd5a7a9f99b46b85e140f9a49b7cc821a7501
Félix <ricardosilva2005@duck.com>
2024-10-29 15:04:41 +0000

    Add project files.

---
+++ Livraria-TI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Livraria_TI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
