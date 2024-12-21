2ae4bb70a6c24c54f48fcf18b6ff23c1b27ac58b
Félix <77445670+Phelliks@users.noreply.github.com>
2024-12-21 18:37:27 +0000

    Mega update para ter o entity :P

--- Livraria-TI/Data/ApplicationDbContext.cs
+++ Livraria-TI/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Livraria_TI.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
