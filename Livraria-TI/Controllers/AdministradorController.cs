887565ebd85d0d906fd33684f9b2b2d975ecaec9
Félix <77445670+Phelliks@users.noreply.github.com>
2024-12-22 14:48:43 +0000

    Updatre no Administador e no produto, e adicão de uma nova aparencia temporaria ao administador aka temos de mudar aquilo :P

--- Livraria-TI/Controllers/AdministradorController.cs
+++ Livraria-TI/Controllers/AdministradorController.cs
@@ -1,109 +1,12 @@
-﻿using Livraria_TI.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using Livraria_TI.Helpers;
-using Livraria_TI.Models.DTOs;
-using Livraria_TI.Models.ViewModels.Livro;
-using Livraria_TI.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Options;
-using System.Security.Claims;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Livraria_TI.Controllers
 {
     public class AdministradorController : Controller
     {
-        private readonly MyOptions _myOptions;
-        private readonly LivroService _LivroService;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private const string ViewPath = "~/Views/Administrador/Produto/";
-        private readonly ILogger<AdministradorController> _logger;
-
-        public AdministradorController(IOptions<MyOptions> myOptions, IHttpContextAccessor httpContextAccessor)
-        {
-            _LivroService = new LivroService(myOptions);
-            _httpContextAccessor = httpContextAccessor;
-        }
-
-        // Lista os produtos
         public IActionResult Index()
         {
-            var model = GetIndexViewModel();
-            return View($"{ViewPath}Index.cshtml", model);
-        }
-
-        private LivroIndexViewModel GetIndexViewModel()
-        {
-            LivroIndexViewModel model = new LivroIndexViewModel
-            {
-                livros = _LivroService.Get().Results
-            };
-            return model;
-        }
-
-        public IActionResult Create()
-        {
-            LivroCreateViewModel model = new LivroCreateViewModel();
-
-            return View($"{ViewPath}Create.cshtml", model);
-        }
-
-        [HttpPost]
-        public IActionResult Create(LivroCreateViewModel model)
-        {
-            LivroDTO dto = new LivroDTO();
-            dto.Titulo = model.Titulo;
-            dto.Editora = model.Editora;
-            dto.Descricao = model.descricao;
-            dto.Preco = model.Preco;
-
-            ExecutionResult<LivroDTO> result = _LivroService.Insert(dto, GetUsername());
-
-            return View($"{ViewPath}Index.cshtml", GetIndexViewModel());
-        }
-        public IActionResult Edit(int id)
-        {
-            LivroEditViewModel model = new LivroEditViewModel();
-
-            LivroDTO produto = _LivroService.Get(id).Results.FirstOrDefault();
-            model.Id_livro = produto.Id_Livro;
-            model.Titulo = produto.Titulo;
-
-            return View($"{ViewPath}Edit.cshtml", model);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(LivroEditViewModel model)
-        {
-            LivroDTO dto = new LivroDTO();
-            dto.Id_Livro = model.Id_livro;
-            dto.Titulo = model.Titulo;
-            dto.Editora = model.Editora;
-            dto.Descricao = model.descricao;
-            dto.Preco = model.Preco;
-
-            ExecutionResult<LivroDTO> result = _LivroService.Update(dto, GetUsername());
-
-            return View(model);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            LivroEditViewModel model = new LivroEditViewModel();
-
-            ExecutionResult<LivroDTO> result = _LivroService.Delete(id);
-
-            return View($"{ViewPath}Index.cshtml", GetIndexViewModel());
-        }
-
-        private string GetUsername()
-        {
-            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
-        }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
