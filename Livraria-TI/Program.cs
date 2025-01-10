ad82f767e69fd5121c0a1b82b6d27c9425262354
Félix <77445670+Phelliks@users.noreply.github.com>
2025-01-10 21:14:20 +0000

    Patch agora tem de dar e ninguem dá push desta merda outra vez...

--- Livraria-TI/Program.cs
+++ Livraria-TI/Program.cs
@@ -1,25 +1,22 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
-using Livraria_TI.Helpers;
 using Livraria_TI.Data;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Livraria_TI.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("LivrariaConnection");
 
-builder.Services.Configure<MyOptions>(myOptions =>
-{
-    myOptions.ConnString = connectionString;
-});
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+// Add Default Identity configuration
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultUI()
     .AddDefaultTokenProviders();
@@ -28,7 +25,10 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
 
-
+builder.Services.Configure<MyOptions>(myOptions =>
+{
+    myOptions.ConnString = connectionString;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,6 +39,7 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
